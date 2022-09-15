8ff29478a3969e88e31f1d9c7bba6a9942501c50
unknown <brundua99@gmail.com>
2022-09-15 11:28:43 -0300

    deploy

--- api/src/app.js
+++ api/src/app.js
@@ -7,10 +7,10 @@ const routes = require('./routes/index.js');
 require('./db.js');
 
 const server = express();
-const cors = require('cors')
+// const cors = require('cors')
 server.name = 'API';
 
-server.use(cors())
+// server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
